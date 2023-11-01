507958bba8992ec74f141bd29d03ed6b8f8044f6
Philippa Davies <philippadavies603@gmail.com>
2023-11-01 23:20:33 +1100

    db name change

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 //Set up configuration
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/LBR-Equestrian');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lbr-equestrian');
 
 module.exports = mongoose.connection;
